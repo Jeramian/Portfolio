27df297909194159c71a002f00886baf46201268
Jeremy Eramian <jeremyleramian@outlook.com>
2024-10-29 12:35:38 -0700

    updated error handling

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -21,45 +21,43 @@ export const Contact = () => {
             });
     
             if (!res.ok) {
-                // Log the response status and statusText for debugging
                 console.error('Response error:', res.status, res.statusText);
                 return;
             }
     
             const result = await res.json();
             console.log('Success:', result);
-        } catch (err: any) {
-            console.error('Error:', err);
+        } catch (err) {
+            if (err instanceof Error) {
+                console.error('Error:', err.message);
+            } else {
+                console.error('Unexpected error:', err);
+            }
         }
     };
-    
 
-  return (
-    <div className='w-full h-auto text-center bg-black py-2 flex flex-col justify-center items-center'>
-      <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-cyan-600 mb-2'>Contact</h1>
-      <div>
-        <form className='rounded px-8 pt-6 pb-8 mb-4' onSubmit={sendMail}>
-            <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
-            <input value={name} onChange={(e) => setName(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='text' placeholder='Name'></input>
-            <br></br>
-            <br></br>
-            <label className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='email' placeholder='Email'></input>
-            <br></br>
-            <br></br>
-            <label className="block text-gray-700 text-sm font-bold mb-2">Subject</label>
-            <input value={subject} onChange={(e) => setSubject(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='text' placeholder='Subject'></input>
-            <br></br>
-            <br></br>
-            <label className="block text-gray-700 text-sm font-bold mb-2">Message</label>
-            <textarea value={message} onChange={(e) => setMessage(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Message'></textarea>
-            <br></br>
-            <br></br>
-            <button className='bg-gradient-to-r from-pink-600 to-cyan-600 text-white rounded-full py-2 px-4' type='submit'>Send</button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className='w-full h-auto text-center bg-black py-2 flex flex-col justify-center items-center'>
+            <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-cyan-600 mb-2'>Contact</h1>
+            <div>
+                <form className='rounded px-8 pt-6 pb-8 mb-4' onSubmit={sendMail}>
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                    <input value={name} onChange={(e) => setName(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='text' placeholder='Name' />
+                    <br /><br />
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='email' placeholder='Email' />
+                    <br /><br />
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Subject</label>
+                    <input value={subject} onChange={(e) => setSubject(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='text' placeholder='Subject' />
+                    <br /><br />
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Message</label>
+                    <textarea value={message} onChange={(e) => setMessage(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Message' />
+                    <br /><br />
+                    <button className='bg-gradient-to-r from-pink-600 to-cyan-600 text-white rounded-full py-2 px-4' type='submit'>Send</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
